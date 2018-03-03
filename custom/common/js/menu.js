94a103fb522fbd60a473858f790ae4af8fd30594
kitaz <kitaz01@gmail.com>
2018-03-03 23:09:40 +0900

    Update Custum Menu.

--- custom/common/js/menu.js
+++ custom/common/js/menu.js
@@ -1,11 +1,14 @@
 $(function() {
+	/* ナビボタンの挙動 */
 	$(".toggle").click(
 		function() {
 			$(this).toggleClass("active");
 			if($(this).hasClass('active')) {
+				$('.row').addClass('active');
 				$('.global_nav').addClass('active');
 			} else {
 				$('.global_nav').removeClass('active');
+				$('.row').removeClass('active');
 			}
 		}
 	);
@@ -15,12 +18,12 @@ $(function() {
 	 */
 	$(".global_nav li a").next().next("a").click(
 		function(){
-			$(this).prev("ul").toggle();
+			$(this).prev("ul").toggle("slow");
 		}
 	);
 	$(".global_nav li a").click(
 		function(){
-			$(this).next("ul").toggle();
+			$(this).next("ul").toggle("slow");
 		}
 	);
 });
